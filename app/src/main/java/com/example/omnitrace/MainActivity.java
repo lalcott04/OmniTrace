6c2a455c4ecdc178747717dd0e4f21fb1b80f66f
Alvo905 <kyleluisalvarez@gmail.com>
2024-09-04 16:02:13 +1000

    Finalised both settings and back button, added a title and status view

--- app/src/main/java/com/example/omnitrace/MainActivity.java
+++ app/src/main/java/com/example/omnitrace/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.omnitrace;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
--- app/src/main/java/com/example/omnitrace/SettingsActivity.java
+++ app/src/main/java/com/example/omnitrace/SettingsActivity.java
@@ -4,18 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SettingsActivity extends Activity {
 
-    Button btnBack;
+    ImageButton backBtn;
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        btnBack = (Button) findViewById(R.id.backBtn);
-        btnBack.setOnClickListener(new View.OnClickListener(){
+        backBtn = (ImageButton) findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
